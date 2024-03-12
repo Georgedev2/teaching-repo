dcf9f928bf2a7d9e239868eae748e63f242a5212
george <v-gnwobodo@digitalocean.com>
2024-03-12 17:23:58 +0100

    my first commit

---
+++ module-system-in-js/main.js
@@ -0,0 +1,15 @@
+export const sumNumbers = (a, b) => {
+  return a + b;
+};
+
+export const school='jk'
+
+
+export const obj={
+    sumNumbers,
+    // school
+}
+
+const key='llll'
+
+const foo=()=>{}
---
+++ module-system-in-js/utils.js
@@ -0,0 +1,9 @@
+
+
+const reverseString=(input)=>{
+return input.split('').reverse().join('').replaceAll(',', '')
+}
+
+export default reverseString;
+
+export const name='kome'
